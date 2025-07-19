fd1bde230726f0802768b02fd612c1f528e23617
Bo Bramer <bramer.bo@gmail.com>
2025-07-18 20:57:53 -0700

    whatever formatting

--- Assets/Scripts/TileGenerator.cs
+++ Assets/Scripts/TileGenerator.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
-public class TileGenerator : MonoBehaviour {
+public class TileGenerator : MonoBehaviour
+{
   [Header("Tile Generation Settings")]
   [SerializeField] private GameObject tilePrefab;
   [SerializeField] private int gridWidth = 64;
@@ -17,69 +18,84 @@ public class TileGenerator : MonoBehaviour {
   [SerializeField] private bool createParentObject = true;
   
   private GameObject gridParent;
-  
-  void Start() {
-    if (generateOnStart) {
+
+  void Start()
+  {
+    if (generateOnStart)
+    {
       GenerateTileGrid();
     }
   }
-  
-  public void GenerateTileGrid() {
+
+  public void GenerateTileGrid()
+  {
     ClearExistingTiles();
-    
-    if (createParentObject) {
+
+    if (createParentObject)
+    {
       gridParent = new GameObject("TileGrid");
       gridParent.transform.position = startPosition;
     }
     
     // Generate tiles
-    for (int x = 0; x < gridWidth; x++) {
-      for (int z = 0; z < gridHeight; z++) {
+    for (int x = 0; x < gridWidth; x++)
+    {
+      for (int z = 0; z < gridHeight; z++)
+      {
         CreateTile(x, z);
       }
     }
-    
-    Debug.Log($"Generated {gridWidth * gridHeight} tiles in a {gridWidth}x{gridHeight} grid");
   }
-  
-  private void CreateTile(int x, int z) {
+
+  private void CreateTile(int x, int z)
+  {
     Vector3 tilePosition = startPosition + new Vector3(x * tileSize, 0, z * tileSize);
     
     GameObject selectedPrefab = tilePrefab;
-    if (useRandomTiles && tileVariations != null && tileVariations.Length > 0) {
+    if (useRandomTiles && tileVariations != null && tileVariations.Length > 0)
+    {
       selectedPrefab = tileVariations[Random.Range(0, tileVariations.Length)];
     }
 
     var rotation = Quaternion.Euler(90, 0, 0);
     
     GameObject tile = Instantiate(selectedPrefab, tilePosition, rotation);
-    
-    if (createParentObject && gridParent != null) {
+
+    if (createParentObject && gridParent != null)
+    {
       tile.transform.SetParent(gridParent.transform);
     }
     
     tile.name = $"Tile_{x}_{z}";
   }
-  
-  public void ClearExistingTiles() {
-    if (gridParent != null) {
+
+  public void ClearExistingTiles()
+  {
+    if (gridParent != null)
+    {
       DestroyImmediate(gridParent);
-    } else {
+    }
+    else
+    {
       GameObject[] existingTiles = GameObject.FindGameObjectsWithTag("Tile");
-      foreach (GameObject tile in existingTiles) {
+      foreach (GameObject tile in existingTiles)
+      {
         DestroyImmediate(tile);
       }
     }
   }
-  
-  public void RegenerateGrid() {
+
+  public void RegenerateGrid()
+  {
     GenerateTileGrid();
   }
-  
-  void Update() {
+
+  void Update()
+  {
   }
-  
-  private void OnDrawGizmosSelected() {
+
+  private void OnDrawGizmosSelected()
+  {
     Gizmos.color = Color.yellow;
     Vector3 gridSize = new Vector3(gridWidth * tileSize, 0, gridHeight * tileSize);
     Gizmos.DrawWireCube(startPosition + gridSize * 0.5f, gridSize);
